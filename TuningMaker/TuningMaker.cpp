4668a0a984a91a86827ef42d636dc67088804b5b
James Byrne <jamespatbyrne60@gmail.com>
2026-01-22 15:30:10 +0800

    removed useless function
    insertDummyNotesInTuning

--- TuningMaker/TuningMaker.cpp
+++ TuningMaker/TuningMaker.cpp
@@ -1,20 +1,11 @@
 #include "PitchSpace.h"
 
-static std::vector<long double> insertDummyNotesInTuning(const std::vector<long double>& tuning, const std::vector<int>& dummyNoteIndecies)
-{
-    auto returnTuning{ tuning };
-    returnTuning.reserve(tuning.size() + dummyNoteIndecies.size());
-
-    
-}
-
 static void initialiseBaseScales()
 {
     using namespace PitchSpaces;
 
     chromatic.addSigniature("major pentatonic", { 0, 2, 4, 7, 9 });
     chromatic.addSigniature("minor pentatonic", { 0, 3, 4, 7, 10 });
-
     chromatic.addSigniature("ionion", { 0, 2, 4, 5, 7, 9, 11 });
     chromatic.addSigniature("dorian", { 0, 2, 3, 5, 7, 9, 10 });
     chromatic.addSigniature("phrygian", { 0, 1, 3, 5, 7, 8, 10 });
@@ -22,10 +13,10 @@ static void initialiseBaseScales()
     chromatic.addSigniature("myxolydian", { 0, 2, 4, 5, 7, 9, 10 });
     chromatic.addSigniature("aolian", { 0, 2, 3, 5, 7, 8, 10 });
     chromatic.addSigniature("locrian", { 0, 1, 3, 5, 6, 8, 10 });
+    chromatic.addSigniature("full", { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 });
 
     neutralSevenEDO.addSigniature("neutral pentatonic A", { 0, 1, 3, 4, 6 });
     neutralSevenEDO.addSigniature("neutral pentatonic B", { 0, 2, 3, 5, 6 });
-
     neutralSevenEDO.addSigniature("full", { 0, 1, 2, 3, 4, 5, 6 });
 }
 
