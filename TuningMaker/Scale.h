372d7b001fabee4078ff5a668703a5c7b57365df
James Byrne <jamespatbyrne60@gmail.com>
2026-01-26 16:19:30 +0800

    Added documentation and global namespace cleanup
    Specifically those moved some functions into a new IntervalPatternMakers namespace

--- TuningMaker/Scale.h
+++ TuningMaker/Scale.h
@@ -3,6 +3,10 @@
 #include "Utilities.h"
 #include <limits>
 
+/*
+  Musically, an interval between two notes is the factor you need to multiply one note by to a arrive at the other. Mathematically,
+  this number is the interval's size, while it's weight represents how "important" it is (howver that is interpreted).
+*/
 struct Interval
 {
     Interval()
@@ -11,117 +15,208 @@ struct Interval
 	{
 	}
 
-    Interval(const long double& s, const long double& w)
+    Interval(const long double& s, const long double& w = 1)
         : size{ clampLongDoubleToLimits(s) }
         , weight{ clampLongDoubleToLimits(w) }
     {}
 
     long double size;
-    long double weight{ 1 };
+    long double weight;
 };
 
+/*
+  A Scale represents a collection of notes as the ideal pattern intervals between those notes.
+  It also contains the logic necessary to produce a tuning of itself, output by tuneScale().
+  It is beyond the scope the documentation of this class to discuss exactly how this tuning
+  is calculated, but it involves treating scales as complete weighted graphs. It may also
+  contain some 'dummy' notes, which are not contained in the scale but are contained in the
+  pitch space containing it (think black notes in the C major scale).
+*/
 class Scale
 {
 public:
+    /*
+      Constructs a nameless scale with a default intervals pattern.
+    */
     Scale();
 
+    /*
+      Constructs a named scale with a default intervals pattern.
+    */
+    Scale(const std::string& n);
+
+    /*
+      Constructs a nameless scale with intervals pattern i. If i has non-triangular dimensions
+      (defined by patternHasTriangularDimensions() function in Utilities.h) then the pattern is
+      default
+    */
     Scale(const std::vector<std::vector<Interval>>& i);
 
+    /*
+       Constructs a named scale with intervals pattern i. If i has non-triangular dimensions
+       (defined by patternHasTriangularDimensions() function in Utilities.h) then the pattern is
+       default.
+    */
     Scale(const std::vector<std::vector<Interval>>& i, const std::string& n);
 
+    /*
+      Returns the number of notes in the scale.
+    */
     inline size_t size() const;
 
+    /*
+      Sets intervalsPattern to newIntervalsPattern if it has triangular dimensions, (defined by
+      patternHasTriangularDimensions() function in Utilities.h).
+    */
     void setIntervalsPattern(const std::vector<std::vector<Interval>>& newIntervalsPattern);
 
+    /*
+      Sets the dummy notes of the scale. In tuning of the scale produced by tuneScale, dummy note
+      tunings will be inserted at these indecies, which are represented by NaN values. Note that
+      the indecies of dummy notes are independent of intervalsPattern and invalid dummy indecies
+      will not be inserted into the final tuning.
+    */
     void setDummyIndecies(const std::vector<int>& newDummyIntervals);
 
+    /*
+      Resets the name of the scale.
+    */
     void setName(const std::string& newName);
 
+    /*
+      Returns the name of the scale if it has one.
+    */
     inline std::string getName() const;
 
+    /*
+      Produces a tuning of the scale. The tuning of the note at index = trueRootNote will always equal 1f
+      Depending on the size of the scale and the weights of it's intervals weightLimit can have a large
+      influence on the time it takes for this function to return.
+    */
     std::vector<double> tuneScale(const int& trueRootNote, const long double& weightLimit = 0) const;
 
 private:
-
+    /*
+      The ideal intervals between all notes in the scale. The interval between notes A and B is equal to
+      the reciporical of the interval between B and A. The weight of both intervals is the same. So,
+      we only need to store the interval between A and B for any value of A or B.
+    */
     std::vector<std::vector<Interval>> intervalsPattern;
-	//dummy notes, which equal -1, are inserted at these indecies. The function of dummy notes is to create spaces
-	//in the scale for notes with 0Hz frequency, which can be fingering scales which are have less than 12 notes per octave
+    /*
+      The indecies of intervals which will always be tuned NaN in tuneScale().
+    */
     std::vector<int> dummyIndecies;
+    /*
+      The name of the scale;
+    */
     std::string name;
 
+    /*
+      Accesses or calculates the value of the interval from noteFrom to noteTo depending on whether or not
+      it is contained in intervalsPattern.
+    */
     Interval getInterval(const int& noteTo, const int& noteFrom) const;
 
+    /*
+      Returns the sum of all notes in notesFrom to noteTo. This is a useful value for tuning calculation.
+    */
     long double sumWeights(const int& noteTo, std::vector<int>& notesFrom) const;
 
+    /*
+      Calculates the tuning of a single note for a scale, assuming a single rootNote.
+    */
     long double makeTuning(const int& rootNote, int& note, const long double& weightLimit) const;
 
+    /*
+      Iteratively traverses across the scale as if it were a graph. Iteration is broken by either finding a
+      path which originates at rootNote, or arriving at a path whose rollingWeight <= weightLimit. Being that
+      this function is called many times, it could be a sensible place to begin optimisation.
+    */
     long double traverseScale(int& lastNote, std::vector<int>& possibleNextNotesInPath, const int& rootNote,
                               const long double& rollingWeight, const long double& weightLimit,
                               const long double& possibleWeightsToNoteSum) const;
-
+    
+    /*
+      Manages calls to makeTuning() for all possible notes and rootNotes, populates size() number of tunings
+      for each note in the scale, and tracks progress of this calculation.
+    */
     std::vector<std::vector<long double>> makePopulatedTunings(const long double& weightLimit) const;
 
+    /*
+      Produces a tuning of the scale from the tunings produced by makePopulatedTunings(), normalised and averaged
+      such that the tuning of the note at index trueRootNote equals 1f
+    */
     std::vector<double> normaliseTuningsAndMakeAverageTuning(std::vector<std::vector<long double>>& tunings,
                                                                   const int& trueRootNote) const;
 
+    /*
+      Inserts NaN at the indecies contained in dummyIndecies if those intervals are valid.
+    */
     std::vector<double> insertDummyNotes(std::vector<double>& tuning) const;
 };
 
-static std::vector<std::vector<Interval>> rangedScaleFractionsToIntervalsWithTenneyWeight(const std::vector<std::vector<Fraction>>& rangedScale, const long double& entropyCurve = 1)
+//All functions in this namespace should return an IntervalPattern which can be used by Scale objects.
+namespace IntervalPatternMakers
 {
-    std::vector<std::vector<Interval>> baseIntervals;
-    baseIntervals.reserve(rangedScale.size());
+    using IntervalsPattern = std::vector<std::vector<Interval>>;
 
-    long double maxWeight{ 0 };
-    for (auto rowItr{ rangedScale.begin() }; rowItr != rangedScale.end(); ++rowItr)
+    /*
+      Produces an IntervalsPattern where all intervals have a weight equal to one over the product of the
+      numerator and denominator (their Tenney Weight), raised to the power of entropyCurve.
+    */
+    static IntervalsPattern
+        rangedScaleFractionsToIntervalsWithTenneyWeight(const std::vector<std::vector<Fraction>>& rangedScale,
+                                                        const long double& entropyCurve = 0)
     {
-        std::vector<Interval> intervalsRow;
+        IntervalsPattern baseIntervals;
+        baseIntervals.reserve(rangedScale.size());
 
-        intervalsRow.reserve(rowItr->size());
-        for (auto fractionItr{ rowItr->begin() }; fractionItr != rowItr->end(); ++fractionItr)
+        long double maxWeight{ 0 };
+        for (auto rowItr{ rangedScale.begin() }; rowItr != rangedScale.end(); ++rowItr)
         {
-            const auto weight{ tenneyWeightOfFraction(*fractionItr, entropyCurve) };
-            intervalsRow.push_back({ fractionItr->toLongDouble(), weight });
+            std::vector<Interval> intervalsRow;
 
-            if (weight > maxWeight)
-                maxWeight = weight;
+            intervalsRow.reserve(rowItr->size());
+            for (auto fractionItr{ rowItr->begin() }; fractionItr != rowItr->end(); ++fractionItr)
+            {
+                const auto weight{ tenneyHeightOfFraction(*fractionItr, entropyCurve) };
+                intervalsRow.push_back({ fractionItr->toLongDouble(), weight });
+
+                if (weight > maxWeight)
+                    maxWeight = weight;
+            }
+
+            baseIntervals.push_back(intervalsRow);
         }
 
-        baseIntervals.push_back(intervalsRow);
+        //normalising weights
+        for (auto& row : baseIntervals)
+            for (auto& Interval : row)
+                Interval.weight /= maxWeight;
+
+        return baseIntervals;
     }
-
-    //normalising weights
-    for (auto& row : baseIntervals)
-        for (auto& Interval : row)
-            Interval.weight /= maxWeight;
-
-    return baseIntervals;
-}
-
-static std::vector<std::vector<Interval>> rangedScaleLongDoubleToIntervalsWithUniformWeight(const std::vector<std::vector<long double>>& rangedScale)
-{
-    std::vector<std::vector<Interval>> baseIntervals;
-    baseIntervals.reserve(rangedScale.size());
-
-    for (auto rowItr{ rangedScale.begin() }; rowItr != rangedScale.end(); ++rowItr)
+    
+    /*
+      Produces an IntervalsPattern where all intervals have equal weight.
+    */
+    static IntervalsPattern
+        rangedScaleLongDoubleToIntervalsWithUniformWeight(const std::vector<std::vector<long double>>& rangedScale)
     {
-        std::vector<Interval> intervalsRow;
+        IntervalsPattern baseIntervals;
+        baseIntervals.reserve(rangedScale.size());
 
-        intervalsRow.reserve(rowItr->size());
-        for (auto fractionItr{ rowItr->begin() }; fractionItr != rowItr->end(); ++fractionItr)
-            intervalsRow.push_back({ *fractionItr, 1});
+        for (auto rowItr{ rangedScale.begin() }; rowItr != rangedScale.end(); ++rowItr)
+        {
+            std::vector<Interval> intervalsRow;
 
-        baseIntervals.push_back(intervalsRow);
+            intervalsRow.reserve(rowItr->size());
+            for (auto fractionItr{ rowItr->begin() }; fractionItr != rowItr->end(); ++fractionItr)
+                intervalsRow.push_back(*fractionItr);
+
+            baseIntervals.push_back(intervalsRow);
+        }
+
+        return baseIntervals;
     }
-
-    return baseIntervals;
-}
-
-static bool intervalsPatternHasValidDimensions(const std::vector<std::vector<Interval>>& intervals)
-{
-    for (auto rowSize{ 0 }; rowSize != intervals.size(); ++rowSize)
-        if (intervals[rowSize].size() != intervals.size() - rowSize)
-            return false;
-
-    return true;
 }
