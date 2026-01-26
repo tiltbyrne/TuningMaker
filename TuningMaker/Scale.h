c9233d055b90ff633e5028765ccf1837b0f57809
James Byrne <jamespatbyrne60@gmail.com>
2026-01-27 00:05:00 +0800

    Made Interval a class pt.3

--- TuningMaker/Scale.h
+++ TuningMaker/Scale.h
@@ -11,24 +11,56 @@
 class Interval
 {
 public:
+    /*
+      Constructs an interval of size and weight = 1;
+    */
     Interval();
 
+    /*
+      Constructs an interval of size s and weight w. These values are clamped to limits if they exceed
+      numeric limits.
+    */
     Interval(const long double& s, const long double& w = 1);
 
+    /*
+      Returns the size of the interval.
+    */
     inline long double getSize() const;
 
+    /*
+      Returns the weight of the interval.
+    */
     inline long double getWeight() const;
 
+    /*
+      Sets the size of the interval and clamps it's value to numeric limits if necassary.
+    */
     void setSize(const long double& newSize);
-
+    
+    /*
+      Sets the size of the interval, clamps it's value to numeric limits, and manages weight = 0 if necassary.
+    */
     void setWeight(const long double& newWeight);
 
+    /*
+      Sets both the size and weight of the interval.
+    */
     void setInterval(const long double& newSize, const long double& newWeight);
 
 private:
+    /*
+      The distance between two notes.
+    */
     long double size;
+    /*
+      The "importance" of the interval.
+    */
     long double weight;
 
+    /*
+      Weights <= 0, which are impossible accordin to the model of tuning used as they can lead to division by
+      0, are set to the numeric limits lowest value for long double.
+    */
     void manageZeroWeight();
 };
 
